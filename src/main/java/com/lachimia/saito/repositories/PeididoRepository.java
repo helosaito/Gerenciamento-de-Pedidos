06dd6d561bfd69d9f3d2afa4b6e09fba6985dc65
unknown <helo.saito@outlook.com>
2026-03-19 19:04:11 -0300

    Repository criado

---
+++ src/main/java/com/lachimia/saito/repositories/PeididoRepository.java
@@ -0,0 +1,7 @@
+package com.lachimia.saito.repositories;
+
+import com.lachimia.saito.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeididoRepository extends JpaRepository<PedidoModel, Long> {
+}
