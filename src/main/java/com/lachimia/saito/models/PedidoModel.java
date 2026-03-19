ce725450536ec69bde4578b5861e604af1749c90
unknown <helo.saito@outlook.com>
2026-03-19 19:02:17 -0300

    model criada

---
+++ src/main/java/com/lachimia/saito/models/PedidoModel.java
@@ -0,0 +1,59 @@
+package com.lachimia.saito.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_pedido")
+public class PedidoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Data do Pedido")
+    private LocalDateTime data;
+
+    @Column(name = "Valor Total do Pedido")
+    private BigDecimal valorTotal;
+
+    @Column(name = "Status do Pedido")
+    private String status;
+
+    public PedidoModel(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
